63bbfb08895b126a956efd5be5bdeae8759a510b
David Buchanan <d@vidbuchanan.co.uk>
2023-12-28 14:38:42 +0000

    fix cleanup logic for real this time, maybe, also add cfg object

--- swargol.py
+++ swargol.py
@@ -5,6 +5,7 @@ from queue import Queue, Empty
 from threading import Thread
 from multiprocessing import Pipe, Process, Event
 from typing import List
+from dataclasses import dataclass
 
 """
 
@@ -45,16 +46,30 @@ which does not hold the GIL.
 
 """
 
-#FB_WIDTH, FB_HEIGHT = (3840, 2160)
-#FB_WIDTH, FB_HEIGHT = (3024, 1890-2)
-FB_WIDTH, FB_HEIGHT = (1920, 1080)
-#FB_WIDTH, FB_HEIGHT = (128, 64)
-WIDTH_PADDING = 16
-VSYNC = 0
-FULLSCREEN = 0
-FRAMESKIP = 31 # 1 = no skipped frames, 2 = every other, 3 = every third, etc.
-NUM_PROCS = 8
-DRYLIFE = 1
+SURFACE_FMT = sdl2.SDL_PIXELFORMAT_ARGB8888
+
+
+@dataclass
+class LifeConfig:
+	fb_width: int
+	fb_height: int
+	width_padding: int
+	vsync: bool
+	fullscreen: bool
+	drylife: bool # whether to use the non-standard "drylife" algorithm.
+	frameskip: int # only render 1-in-n frames to the screen
+	num_procs: int # degree of parallelism
+
+#cfg.fb_width, cfg.fb_height = (3840, 2160)
+#cfg.fb_width, cfg.fb_height = (3024, 1890-2-64)
+#cfg.fb_width, cfg.fb_height = (1920, 1080)
+#cfg.fb_width, cfg.fb_height = (128, 64)
+#cfg.width_padding = 16
+#VSYNC = 1
+#cfg.fullscreen = 0
+#cfg.frameskip = 1 # 1 = no skipped frames, 2 = every other, 3 = every third, etc.
+#cfg.num_procs = 8
+#cfg.drylife = 1
 
 # return all remaining items in a queue
 def queue_purge(queue: Queue):
@@ -65,19 +80,19 @@ def queue_purge(queue: Queue):
 		pass
 
 
-def life_thread(stopped, width, height, packed_pipe, pipe_top, pipe_bottom):
+def life_thread(cfg: LifeConfig, i, width, height, packed_pipe, pipe_top, pipe_bottom):
 	WIDTH, HEIGHT = width, height
 
-	STRIDE = WIDTH + WIDTH_PADDING
+	STRIDE = WIDTH + cfg.width_padding
 	STATE_BYTE_LENGTH = (STRIDE * HEIGHT) // 2
 	COLSHIFT = STRIDE * 4
 	WRAPSHIFT = STRIDE * HEIGHT * 4
 	BIAS = (STRIDE + 2) * 4
 
 	MASK_1 = int.from_bytes(b"\x11" * STATE_BYTE_LENGTH, "little") << BIAS
-	MASK_CANVAS = int.from_bytes((b"\x11" * (WIDTH // 2) + b"\x00" * (WIDTH_PADDING // 2)) * HEIGHT, "little") << BIAS
-	MASK_WRAP_LEFT = int.from_bytes((b"\x11" * ((WIDTH_PADDING // 2) // 2) + b"\x00" * ((WIDTH - WIDTH_PADDING // 2) // 2) + b"\x00" * (WIDTH_PADDING // 2)) * (HEIGHT + 2), "little") << (2 * 4)
-	MASK_WRAP_RIGHT = int.from_bytes((b"\x00" * ((WIDTH - WIDTH_PADDING // 2) // 2) + b"\x11" * ((WIDTH_PADDING // 2) // 2) + b"\x00" * (WIDTH_PADDING // 2)) * (HEIGHT + 2), "little") << (2 * 4)
+	MASK_CANVAS = int.from_bytes((b"\x11" * (WIDTH // 2) + b"\x00" * (cfg.width_padding // 2)) * HEIGHT, "little") << BIAS
+	MASK_WRAP_LEFT = int.from_bytes((b"\x11" * ((cfg.width_padding // 2) // 2) + b"\x00" * ((WIDTH - cfg.width_padding // 2) // 2) + b"\x00" * (cfg.width_padding // 2)) * (HEIGHT + 2), "little") << (2 * 4)
+	MASK_WRAP_RIGHT = int.from_bytes((b"\x00" * ((WIDTH - cfg.width_padding // 2) // 2) + b"\x11" * ((cfg.width_padding // 2) // 2) + b"\x00" * (cfg.width_padding // 2)) * (HEIGHT + 2), "little") << (2 * 4)
 	MASK_NOT_3 = MASK_1 * (15 ^ 3)
 	MASK_NOT_4 = MASK_1 * (15 ^ 4)
 	MASK_NOT_7 = MASK_1 * (15 ^ 7)
@@ -88,9 +103,10 @@ def life_thread(stopped, width, height, packed_pipe, pipe_top, pipe_bottom):
 	else:
 		# glider test
 		seed_bytes = bytearray(STATE_BYTE_LENGTH)
-		seed_bytes[(STRIDE//2)*4+3:(STRIDE//2)*4+3+2] = b"\x10\x00"
-		seed_bytes[(STRIDE//2)*5+3:(STRIDE//2)*5+3+2] = b"\x00\x01"
-		seed_bytes[(STRIDE//2)*6+3:(STRIDE//2)*6+3+2] = b"\x11\x01"
+		if i == 0:
+			seed_bytes[(STRIDE//2)*4+3:(STRIDE//2)*4+3+2] = b"\x10\x00"
+			seed_bytes[(STRIDE//2)*5+3:(STRIDE//2)*5+3+2] = b"\x00\x01"
+			seed_bytes[(STRIDE//2)*6+3:(STRIDE//2)*6+3+2] = b"\x11\x01"
 
 	state = (int.from_bytes(seed_bytes, "little") << BIAS) & MASK_CANVAS
 	pipe_top.send_bytes(seed_bytes[:STRIDE//2]) # send up our top row
@@ -98,7 +114,7 @@ def life_thread(stopped, width, height, packed_pipe, pipe_top, pipe_bottom):
 
 	framectr = 0
 	try:
-		while not stopped.is_set():
+		while True: # we'll keep going until killed
 			"""
 			if we include ourself as a neighbor:
 			alive = (exactly 3 neighbors) or (alive and 4 neighbors)
@@ -125,11 +141,13 @@ def life_thread(stopped, width, height, packed_pipe, pipe_top, pipe_bottom):
 			has_4_neighbors &= has_4_neighbors >> 2  # fold in half
 			has_4_neighbors &= has_4_neighbors >> 1  # fold in half again
 
-			if DRYLIFE:
-				# check if there are exactly 4 neighbors
+			if cfg.drylife:
+				# check if there are exactly 7 neighbors
 				has_7_neighbors = summed ^ MASK_NOT_7 # at this point, a value of all 1s means it was initially 7
 				has_7_neighbors &= has_7_neighbors >> 2  # fold in half
 				has_7_neighbors &= has_7_neighbors >> 1  # fold in half again
+				
+				# variable names here are misleading...
 				has_7_neighbors &= ~state
 				has_3_neighbors |= has_7_neighbors
 
@@ -143,72 +161,66 @@ def life_thread(stopped, width, height, packed_pipe, pipe_top, pipe_bottom):
 			pipe_bottom.send_bytes(packed_state[-(STRIDE//2+1):])
 
 			framectr += 1
-			if framectr % FRAMESKIP:
+			if framectr % cfg.frameskip:
 				continue
 
 			packed_pipe.send_bytes(packed_state)
-		
-		print("life_thread: graceful exit")
-		packed_pipe.close()
-		# we can't close pipe_top/pipe_bottom here because another process might be waiting on it
-	
+
 	except KeyboardInterrupt:
-		print("SIGINT")
+		print("life_thread SIGINT")
+		while True:
+			packed_pipe.send_bytes(bytes(STATE_BYTE_LENGTH)) # unblock any readers, until we get killed
 
 
-SURFACE_FMT = sdl2.SDL_PIXELFORMAT_ARGB8888
-
-
-def blit_thread(stopped: Event, life_stopped: Event, packed_queue, blitted_queue: Queue):
+def blit_thread(cfg: LifeConfig, i, stopped: Event, packed_queue, blitted_queue: Queue):
 	while not stopped.is_set():
 		packed_frame = packed_queue.recv_bytes() # this needs to stay in scope until SDL_ConvertSurfaceFormat is complete!
 		surface = sdl2.SDL_CreateRGBSurfaceWithFormatFrom(
 			packed_frame,
-			FB_WIDTH, FB_HEIGHT // NUM_PROCS, # XXX: use config
+			cfg.fb_width, cfg.fb_height // cfg.num_procs, # XXX: use config
 			4, # depth
-			(FB_WIDTH + WIDTH_PADDING) // 2, # pitch
+			(cfg.fb_width + cfg.width_padding) // 2, # pitch
 			sdl2.SDL_PIXELFORMAT_INDEX4LSB
 		)
-		sdl2.SDL_SetPaletteColors(surface.contents.format.contents.palette, sdl2.SDL_Color(0, 0, 0, 255), 0, 1)
+		sdl2.SDL_SetPaletteColors(surface.contents.format.contents.palette, sdl2.SDL_Color(40, 40, 40, 255), 0, 1)
 		sdl2.SDL_SetPaletteColors(surface.contents.format.contents.palette, sdl2.SDL_Color(255, 255, 255, 255), 1, 1)
 		blitted_queue.put(sdl2.SDL_ConvertSurfaceFormat(surface, SURFACE_FMT, 0))
 		sdl2.SDL_FreeSurface(surface)
 	
-	print("blit_thread: graceful exit")
-	life_stopped.set() # stop our corresponding life process
+	print(f"blit_thread {i}: graceful exit")
 	packed_queue.recv_bytes() # do a final read to un-block the writer
 	packed_queue.close() # close our end of the Pipe
 
 
-def gui_thread(blitted_queues: List[Queue]):
+def gui_thread(cfg: LifeConfig, blitted_queues: List[Queue]):
 	if sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO) < 0:
 		raise Exception("Failed to init SDL2")
 
 	window = sdl2.SDL_CreateWindow(
 		b"pysdl2 framebuffer test",
 		sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED,
-		FB_WIDTH, FB_HEIGHT,
+		cfg.fb_width, cfg.fb_height,
 		sdl2.SDL_WINDOW_SHOWN
 	)
 
 	if not window:
 		raise Exception("Failed to create SDL2 Window")
 
-	if FULLSCREEN:
+	if cfg.fullscreen:
 		sdl2.SDL_SetWindowFullscreen(window, sdl2.SDL_WINDOW_FULLSCREEN)
 
-	renderer = sdl2.SDL_CreateRenderer(window, -1, sdl2.SDL_RENDERER_ACCELERATED | (sdl2.SDL_RENDERER_PRESENTVSYNC if VSYNC else 0))
+	renderer = sdl2.SDL_CreateRenderer(window, -1, sdl2.SDL_RENDERER_ACCELERATED | (sdl2.SDL_RENDERER_PRESENTVSYNC if cfg.vsync else 0))
 
 	if not renderer:
 		raise Exception("Failed to create SDL2 Renderer")
 
 	textures = []
-	for _ in range(NUM_PROCS):
+	for _ in range(cfg.num_procs):
 		texture = sdl2.SDL_CreateTexture(
 			renderer,
 			SURFACE_FMT,
 			sdl2.SDL_TEXTUREACCESS_STREAMING,
-			FB_WIDTH, FB_HEIGHT // NUM_PROCS
+			cfg.fb_width, cfg.fb_height // cfg.num_procs
 		)
 
 		if not texture:
@@ -235,15 +247,15 @@ def gui_thread(blitted_queues: List[Queue]):
 			surface = surface_queue.get()
 			sdl2.SDL_UpdateTexture(texture, None, surface.contents.pixels, surface.contents.pitch)
 			sdl2.SDL_FreeSurface(surface)
-			sdl2.SDL_RenderCopy(renderer, texture, None, sdl2.SDL_Rect(0, (FB_HEIGHT//NUM_PROCS)*i, FB_WIDTH, FB_HEIGHT//NUM_PROCS))
+			sdl2.SDL_RenderCopy(renderer, texture, None, sdl2.SDL_Rect(0, (cfg.fb_height//cfg.num_procs)*i, cfg.fb_width, cfg.fb_height//cfg.num_procs))
 
 		sdl2.SDL_RenderPresent(renderer)
 
 		now = time.time()
 		fps = len(prev_times)/(now-prev_times[prev_time_i])
-		msg = f"{fps:.1f}fps ({fps*FRAMESKIP:.1f}tps)"
+		msg = f"{fps:.1f}fps ({fps*cfg.frameskip:.1f}tps)"
 		#print(msg)
-		sdl2.SDL_SetWindowTitle(window, ("pyswargol - " + msg).encode())
+		sdl2.SDL_SetWindowTitle(window, (f"pyswargol - {cfg.fb_width}x{cfg.fb_height} - " + msg).encode())
 		prev_times[prev_time_i] = now
 		prev_time_i = (prev_time_i + 1) % len(prev_times)
 
@@ -253,61 +265,45 @@ def gui_thread(blitted_queues: List[Queue]):
 	sdl2.SDL_Quit()
 
 
-if __name__ == "__main__":
+def main(cfg: LifeConfig):
 	stopped = Event()
-	life_stopped = [Event() for _ in range(NUM_PROCS)]
 
-	assert((FB_HEIGHT % NUM_PROCS) == 0)
+	assert((cfg.fb_height % cfg.num_procs) == 0)
 
-	blitted_queues = [Queue(1) for _ in range(NUM_PROCS)]
+	blitted_queues = [Queue(1) for _ in range(cfg.num_procs)]
 
-	wraparound_pipes = [Pipe() for _ in range(NUM_PROCS)]
-	packed_result_pipes = [Pipe(duplex=False) for _ in range(NUM_PROCS)]
+	wraparound_pipes = [Pipe() for _ in range(cfg.num_procs)]
+	packed_result_pipes = [Pipe(duplex=False) for _ in range(cfg.num_procs)]
 	life_procs = [
-		Process(target=life_thread, args=[life_stopped[i], FB_WIDTH, FB_HEIGHT // NUM_PROCS, packed_result_pipes[i][1], wraparound_pipes[i][0], wraparound_pipes[(i+1)%NUM_PROCS][1]])
-		for i in range(NUM_PROCS)
+		Process(target=life_thread, args=[cfg, i, cfg.fb_width, cfg.fb_height // cfg.num_procs, packed_result_pipes[i][1], wraparound_pipes[i][0], wraparound_pipes[(i+1)%cfg.num_procs][1]])
+		for i in range(cfg.num_procs)
 	]
 	for proc in life_procs:
 		proc.start()
 
 	blitter_threads = [
-		Thread(target=blit_thread, args=[stopped, life_stopped[i], packed_result_pipes[i][0], blitted_queues[i]])
-		for i in range(NUM_PROCS)
+		Thread(target=blit_thread, args=[cfg, i, stopped, packed_result_pipes[i][0], blitted_queues[i]])
+		for i in range(cfg.num_procs)
 	]
 	for thread in blitter_threads:
 		thread.start()
 
 	try:
-		gui_thread(blitted_queues)
+		gui_thread(cfg, blitted_queues)
 	except KeyboardInterrupt:
 		print("Looks like you pressed Ctrl+C!")
-		print("For a more graceful exit, hit Esc or close the SDL window.")
-		os._exit(-1)
 
 	# The shutdown process is surprisingly fiddly to get right, without deadlocks
 	print("Shutting down...")
-
-	# wait for all queues to fill up, so we're in a deterministic state for cleanup
-	while not (
-		all(q.full() for q in blitted_queues) and
-		all(a.poll() for a, _ in packed_result_pipes)
-	):
-		print("Waiting for queues to fill...")
-		time.sleep(0.01)
-	
-	# actually, .poll() isn't enough. This is too much of a headache to solve
-	# properly, so here's a load-bearing sleep.
-	# perhaps using a barrier to keep the Life threads in lock-step would help.
-	time.sleep(0.5)
 
 	stopped.set() # tell the blitter threads to stop
 
 	# unblock the blitters so they can "notice" the stop event
 	for queue in blitted_queues:
-		sdl2.SDL_FreeSurface(queue.get())
+		for surface in queue_purge(queue):
+			sdl2.SDL_FreeSurface(surface)
 
 	# wait for blitter threads to exit gracefully.
-	# they'll also tell their respective Life thread to stop.
 	for thread in blitter_threads:
 		thread.join()
 
@@ -318,9 +314,10 @@ if __name__ == "__main__":
 		for surface in queue_purge(queue):
 			sdl2.SDL_FreeSurface(surface)
 
-	# wait for the processes to exit
+	# forcefully kill the life procs, trying to do this
+	# cleanly without races is proving too difficult...
 	for proc in life_procs:
-		proc.join()
+		proc.kill()
 
 	print("Stopped life procs.")
 
@@ -331,3 +328,17 @@ if __name__ == "__main__":
 		b.close()
 
 	print("Bye!")
+
+
+if __name__ == "__main__":
+	cfg = LifeConfig(
+		fb_width=1920,
+		fb_height=1080,
+		width_padding=16,
+		vsync=True,
+		fullscreen=False,
+		drylife=True,
+		frameskip=1,
+		num_procs=8
+	)
+	main(cfg)
